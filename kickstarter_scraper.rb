95a3d2a04b19521fba75d9276a047d6abb1b09d1
David Little <littledavid175@gmail.com>
2019-08-12 19:51:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,9 +13,6 @@ def create_project_hash
   projects
   end
  
-  # return the projects hash
-  projects
-end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
