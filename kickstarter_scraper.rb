05a9b6b59bb95b1b75629f2552ddab9242a2962b
David Little <littledavid175@gmail.com>
2019-08-12 19:50:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,9 +6,11 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
-  kickstarter.css("li.project.grid_4").each do |project|
-    
-  end
+    kickstarter.css("li.project.grid_4").each do |project|
+     title = project.css("h2.bbcard_name strong a").text
+     projects[title.to_sym] = {}
+   end
+  projects
   end
  
   # return the projects hash
