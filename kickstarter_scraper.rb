b205f016e031fa9bed32865625036f521ec086ed
David Little <littledavid175@gmail.com>
2019-08-12 19:48:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,4 +11,5 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+
 create_project_hash
