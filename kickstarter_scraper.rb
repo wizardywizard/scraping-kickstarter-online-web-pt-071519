58417d3580783d93cfb883f2e6ca779def8ea450
David Little <littledavid175@gmail.com>
2019-08-12 19:50:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    
   end
   end
  
